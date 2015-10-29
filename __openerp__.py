21fd599b2ff95ccb5027e1379ae1dc74d4c1b54c
mayank-browseinfo <mayankpatel@browseinfo.in>
2015-10-29 10:29:53 +0530

    hiii

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+        'name': 'product_new',
+        'version': '1.0',
+        'category': 'Product',
+        'sequence': 14,
+
+        'description': """
+        This Module contains the basic requirement of school Managenent.
+        """,
+        'author': 'krina',
+        'website': 'http://www.com',
+        'depends': ['base', 'sale'],
+        'data': [
+               
+                ],
+        'demo': [],
+        'test': [
+                        ],
+        'installable': True,
+        'auto_install': False,
+        'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
---
+++ product.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import osv
+from openerp.osv import fields
+
+class product_product(osv.osv):
+    _inherit ='product.product'
+    _columns = { 
+                  } 
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
